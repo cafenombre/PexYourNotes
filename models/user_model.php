86565fe569eff09c1a376aa574f31ef06bf0ab00
cafenombre <tristan.glotin@gmail.com>
2019-11-28 00:37:33 +0100

    finished user managment

--- models/user_model.php
+++ models/user_model.php
@@ -8,15 +8,23 @@ $usrname_test2 = "pingux2";
 $username = "pingux".rand(0,100000000);
 $pwd = "password";
 
-//print_r(add_user($usrname_test, $pwd, $conn));
+//$test = add_user($usrname_test, $pwd, $conn);
+//print_r($test);
+
+//echo ". pwd checkin = ".password_verify($pwd, $test)." --- ";
+
+//echo check_login($usrname_test, $pwd, $conn);
+
+//echo username_exists($usrname_test, $conn);
+
 //echo "usr id =".get_userid($usrname_test,$conn)[0][0];
 //print_r(get_userid($usrname_test,$conn));
-$id = get_userid($usrname_test,$conn)[0][0];
+//$id = get_userid($usrname_test,$conn)[0][0];
 //print_r(update_username($id, $username ,$conn));
 //delete_user($id, $conn);
 //update_password($id, "pwd_updated", $conn);
 //print_r(check_login($usrname_test, $pwd, $conn));
-print_r(check_login($usrname_test, "password", $conn ));
+
 //print_r(get_all_users($conn));
 //$crypt = encryp_pwd("password");
 //echo password_verify("password  ", $crypt);
@@ -35,22 +43,26 @@ function check_login($username, $password, $conn){
     //Call the database
     $result = mysqli_fetch_row($conn->query($sql)); 
     // check password
-    echo $password;
-    echo $result[2];
-    echo "isitworking : ".password_verify($password, $result[2]);
-    /*if(password_verify($password, $result[2]))
-        echo "oui";*/
-        
-    //return mysqli_fetch_row($result)[0]; //return single value or null if empty
-    return 0;
+
+    return password_verify($password, $result[2]);
+}
+
+//check if username exists
+function username_exists($username, $conn){
+    $sql = "SELECT * FROM `users` WHERE `username` = '".$username."'";
+
+    $result = mysqli_fetch_row($conn->query($sql)); 
+
+    return isset($result);
 }
 
 // ------------------- DATABASE ACTIONS ------------------------
 
 //Create a user   
 function add_user($name, $pwd, $conn){
+    $pwd_en = encryp_pwd($pwd);
     $sql = "INSERT INTO `users` (`username`, `password`)
-    VALUES ('".$name."', '".encryp_pwd($pwd)."');";
+    VALUES ('".$name."', '".$pwd_en."');";
     $result = $conn->query($sql);
 
     return 0;
